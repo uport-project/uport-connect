2b54c1b8f6e5e320a9c401e8041a84262b08e8f4
Joel Torstensson <oed3933@gmail.com>
2016-07-02 15:38:09 -0400

    Outline for QR integration

---
+++ util/qrdisplay.js
@@ -0,0 +1,14 @@
+var qr = require('qr-js');
+
+//var test = qr.canvas('asdf');
+
+module.exports = QRDisplay;
+
+function QRDisplay() {
+}
+
+QRDisplay.prototype.openQr(data) {
+}
+
+QRDisplay.prototype.closeQr(data) {
+}
