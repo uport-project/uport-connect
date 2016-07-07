57bc9b603b6f311afca121d959d70050afa95120
Joel Torstensson <oed3933@gmail.com>
2016-07-07 10:32:13 +0200

    Updated example

--- example.js
+++ example.js
@@ -3,21 +3,42 @@ var Web3 = require('web3')
 
 var web3 = new Web3();
 
-web3.setProvider(new web3.providers.HttpProvider("http://localhost:8545"));
+//web3.setProvider(new web3.providers.HttpProvider("http://localhost:8545"));
+
+var statusContract = web3.eth.contract([{"constant":false,"inputs":[{"name":"status","type":"string"}],"name":"updateStatus","outputs":[],"type":"function"},{"constant":false,"inputs":[{"name":"addr","type":"address"}],"name":"getStatus","outputs":[{"name":"","type":"string"}],"type":"function"}]);
+var status = statusContract.at("0x60dd15dec1732d6c8a6125b21f77d039821e5b93");
+
 
 var Uport = require('./index.js')
 
-var uport = new Uport()
+var uport = new Uport("Simple example")
 
 uport.setWeb3(web3)
 
-
 web3.eth.getCoinbase(function(err, address) {
-    console.log("address: " + address)
-    web3.eth.defaultAccount = address
+  console.log("address: " + address)
+  web3.eth.defaultAccount = address
 
-    web3.eth.sendTransaction({value: 10, to: '0xd611e4d19949ceb79ee04c88aff5fc6c879e0e1e', data: "0x875765ab3e"}, function(err, txHash) {
-        console.log("txHash: " + txHash);
-    });
+  //web3.eth.sendTransaction({value: 10, to: '0xd611e4d19949ceb79ee04c88aff5fc6c879e0e1e'}, function(err, txHash) {
+  //console.log("txHash: " + txHash);
+  //});
+
+  status.updateStatus("lalalalla", function(e, r) {
+    console.log("Waiting for tx to be mined.");
+
+    waitForMined(r, {blockNumber: null});
+  })
 });
 
+var waitForMined = function(txHash, res) {
+  if (res.blockNumber) {
+    status.getStatus.call(web3.eth.defaultAccount, function(e, r) {
+      console.log("My status is: " + r);
+    });
+  }
+  else {
+    web3.eth.getTransaction(txHash, function(e, r) {
+      waitForMined(txHash, r);
+    });
+  }
+}
