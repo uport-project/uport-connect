7bd5c34e377f7043965f922a863b08fb0c2dbd26
Joel Torstensson <oed3933@gmail.com>
2016-07-18 17:17:47 +0200

    Added tests for mobile data flow

--- test/msgServer.js
+++ test/msgServer.js
@@ -3,6 +3,7 @@ const xhr = process.browser ? require('xhr') : require('request')
 const MsgServer = require('../lib/msgServer.js');
 
 const chasquiUrl = 'https://chasqui.uport.me/';
+const testHref = 'http://not.real.url/';
 var msgServer;
 var topic1;
 var topic2;
@@ -10,56 +11,100 @@ var topic2;
 describe("MsgServer", function() {
   this.timeout(10000);
 
-  before(() => {
-    msgServer = new MsgServer(chasquiUrl);
-  });
-
-  it("Creates new topics correctly", (done) => {
-    topic1 = msgServer.newTopic('address');
-    assert.equal(topic1.name, 'address');
-    assert.equal(topic1.id.length, 16);
-    assert.equal(topic1.url, chasquiUrl + 'addr/' + topic1.id);
-
-    topic2 = msgServer.newTopic('tx');
-    assert.equal(topic2.name, 'tx');
-    assert.equal(topic2.id.length, 16);
-    assert.equal(topic2.url, chasquiUrl + 'tx/' + topic2.id);
-
-    done()
-  });
-
-  it("Correctly polls for data", (done) => {
-    var data = '0x123456789';
-    msgServer.pollForResult(topic1, function(err, res) {
-      assert.equal(res, data, "Should get correct data from server.");
-      assert.isNull(err);
-      done();
+  describe("On desktop", () => {
+    before(() => {
+      msgServer = new MsgServer(chasquiUrl, false);
     });
-    setTimeout(
-      postData.bind(null, topic1, data),
-      3000
-    );
-  });
 
-  it("Gives error if polling yeilds error", (done) => {
-    var data = "some weird error";
-    msgServer.pollForResult(topic2, function(err, res) {
-      assert.equal(err, data);
-      assert.isUndefined(res);
-      done();
+    it("Creates new topics correctly", (done) => {
+      topic1 = msgServer.newTopic('address');
+      assert.equal(topic1.name, 'address');
+      assert.equal(topic1.id.length, 16);
+      assert.equal(topic1.url, chasquiUrl + 'addr/' + topic1.id);
+
+      topic2 = msgServer.newTopic('tx');
+      assert.equal(topic2.name, 'tx');
+      assert.equal(topic2.id.length, 16);
+      assert.equal(topic2.url, chasquiUrl + 'tx/' + topic2.id);
+
+      done()
     });
-    var errorTopic = topic2;
-    errorTopic.name = "error";
-    setTimeout(
-      postData.bind(null, errorTopic, data),
-      3000
-    );
+
+    it("Correctly polls for data", (done) => {
+      var data = '0x123456789';
+      msgServer.waitForResult(topic1, function(err, res) {
+        assert.equal(res, data, "Should get correct data from server.");
+        assert.isNull(err);
+        done();
+      });
+      setTimeout(
+        postData.bind(null, topic1, data),
+        3000
+      );
+    });
+
+    it("Gives error if polling yeilds error", (done) => {
+      var data = "some weird error";
+      msgServer.waitForResult(topic2, function(err, res) {
+        assert.equal(err, data);
+        assert.isUndefined(res);
+        done();
+      });
+      var errorTopic = topic2;
+      errorTopic.name = "error";
+      setTimeout(
+        postData.bind(null, errorTopic, data),
+        3000
+      );
+    });
+
+    it("Has cleared topic", (done) => {
+      postData(topic1, "0x234", (e,r,b) => {
+        assert.equal(b, topic1.id + " not found!");
+        done();
+      });
+    });
   });
 
-  it("Has cleared topic", (done) => {
-    postData(topic1, "0x234", (e,r,b) => {
-      assert.equal(b, topic1.id + " not found!");
-      done();
+  describe("On Mobile", () =>{
+    before(() => {
+      msgServer = new MsgServer(chasquiUrl, true);
+      global.location = {href: testHref};
+      global.window = {};
+    });
+
+    it("Creates new topics correctly", (done) => {
+      topic1 = msgServer.newTopic('address');
+      assert.equal(topic1.name, 'address');
+      assert.equal(topic1.url, testHref);
+
+      topic2 = msgServer.newTopic('tx');
+      assert.equal(topic2.name, 'tx');
+      assert.equal(topic2.url, testHref);
+
+      done()
+    });
+
+    it("Correctly waits for data", (done) => {
+      var data = '0x123456789';
+      msgServer.waitForResult(topic1, function(err, res) {
+        assert.equal(res, data, "Should get correct data.");
+        assert.isNull(err);
+        done();
+      });
+      location.hash = "#" + topic1.name + "=" + data;
+      global.window.onhashchange();
+    });
+
+    it("Gives error if error posted", (done) => {
+      var data = "some weird error";
+      msgServer.waitForResult(topic2, function(err, res) {
+        assert.equal(err, data);
+        assert.isUndefined(res);
+        done();
+      });
+      location.hash = "#error=" + data;
+      global.window.onhashchange();
     });
   });
 });
