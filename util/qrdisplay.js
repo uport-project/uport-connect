13341be426251e6f55c7dbe1a6dc74c938eb2ab6
Joel Torstensson <oed3933@gmail.com>
2016-07-19 14:58:51 +0200

    Added a default QR display

--- util/qrdisplay.js
+++ util/qrdisplay.js
@@ -1,6 +1,4 @@
-//var qr = require('qr-js');
-
-//var test = qr.canvas('asdf');
+var qr = require('qr-image');
 
 module.exports = QRDisplay;
 
@@ -8,9 +6,40 @@ function QRDisplay() {
 }
 
 QRDisplay.prototype.openQr = function(data) {
-  console.log("QR with data: " + data);
+  var uportQR = this.getUportQRDisplay();
+  uportQR.style.display = "block";
+
+  var pngBuffer = qr.imageSync(data, {type: 'png', margin: 2});
+  var dataUri = 'data:image/png;base64,' + pngBuffer.toString('base64');
+  var qrImg = uportQR.children[0].children[0];
+  qrImg.setAttribute("src", dataUri);
 }
 
 QRDisplay.prototype.closeQr = function() {
-  console.log("Closing QR-code");
+  var uportQR = this.getUportQRDisplay();
+  uportQR.style.display = "none";
+}
+
+QRDisplay.prototype.getUportQRDisplay = function() {
+  var bg = document.getElementById("uport-qr");
+  if (bg) return bg;
+
+  var bg = document.createElement("div");
+  bg.setAttribute("id", "uport-qr");
+  bg.setAttribute("style", "position:fixed;top: 0;width:100%;height:100%;z-index:100;background-color:rgba(0,0,0,0.5);text-align:center;");
+
+  var box = document.createElement("div");
+  box.setAttribute("style", "position:relative;top:50%;display:inline-block;z-index:101;background:#fff;transform:translateY(-50%);margin:0 auto;padding:20px");
+
+  var text = document.createElement("p");
+  text.innerHTML = "Please scan with uport app";
+
+  var qrImg = document.createElement("img");
+  qrImg.setAttribute("style", "z-index:102;");
+
+  box.appendChild(qrImg);
+  box.appendChild(text);
+  bg.appendChild(box);
+  document.body.appendChild(bg);
+  return bg;
 }
