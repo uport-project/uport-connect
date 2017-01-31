3e702264966a7bd485d542dcca1d51a8b3b3215c
Zach Ferland <zachferland@gmail.com>
2017-01-31 15:35:32 -0500

    feat(uport): add general request object, and requests with default QR flow

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { Uport } from './uport'
 import { QRUtil } from './util/qrdisplay'
 import { Registry, PublicPersona } from 'uport-persona'
-export { Uport, PublicPersona, Registry, QRUtil }
+import { UportQRDisplay } from './uportQRDisplay'
+export { Uport, PublicPersona, Registry, QRUtil, UportQRDisplay }
