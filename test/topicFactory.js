e321c41c41b2048b8c5b4e95f68a93ef77e18ff3
Pelle Braendgaard <pelle@stakeventures.com>
2017-02-01 22:19:15 -0600

    test(topicFactory): fix a few failures

--- test/topicFactory.js
+++ test/topicFactory.js
@@ -3,7 +3,6 @@ import request from 'request'
 import nets from 'nets'
 import TopicFactory from '../lib/topicFactory.js'
 
-const chasquiUrl = 'https://chasqui.uport.me/api/v1/topic/'
 let topicFactory
 
 describe('TopicFactory', function () {
@@ -11,7 +10,7 @@ describe('TopicFactory', function () {
 
   describe('On desktop', function () {
 
-    before(function () { topicFactory = TopicFactory(false, chasquiUrl, 500) })
+    before(function () { topicFactory = TopicFactory(false, 500) })
 
     it('Correctly polls for data', (done) => {
       let data = '0x123456789'
@@ -25,7 +24,7 @@ describe('TopicFactory', function () {
       })
       setTimeout(
         () => postData(topic.url, 'access_token', data),
-        3000
+        1000
       )
     })
 
@@ -41,21 +40,24 @@ describe('TopicFactory', function () {
       })
       setTimeout(
         () => postData(topic.url, 'error', data),
-        3000
+        1000
       )
     })
 
     it('Has cleared topic', (done) => {
       const topic = topicFactory('access_token')
       topic.then((res) => {
-        postData(topic.url, 'access_token', '0x234', (e, r, b) => {
-          assert.equal(b.data.id, 'not found')
-          done()
-        })
+        setTimeout(
+          () => postData(topic.url, 'access_token', '0x234', (e, r, b) => {
+            console.log(b)
+            assert.equal(b.data.id, 'not found')
+            done()
+          }),
+        500)
       })
       setTimeout(
         () => postData(topic.url, 'access_token', '0x123'),
-        3000
+        1000
       )
     })
   })
