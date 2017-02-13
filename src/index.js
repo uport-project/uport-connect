e7cec638c977fe842be5178f8993a96ecf48eb79
Zach Ferland <zachferland@gmail.com>
2017-02-13 13:52:52 -0500

    feat(connect): separate Connect and create ConnectCore without web3
    allows developers to use uport-connect without the large web3 dependency, whether with dist file or
    
    if developer creating their own build

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Connect from './Connect'
+import ConnectCore from './ConnectCore'
 import { getQRDataURI, closeQr, openQr } from './util/qrdisplay'
 const QRUtil = { getQRDataURI, closeQr, openQr }
 import { SimpleSigner, Credentials } from 'uport'
-export { Connect, QRUtil, SimpleSigner, Credentials }
+export { Connect, ConnectCore, QRUtil, SimpleSigner, Credentials }
