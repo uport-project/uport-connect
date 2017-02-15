a5c168ce153fae9722199b80bf4a1f84bad8bcb7
Zach Ferland <zachferland@gmail.com>
2017-02-15 11:02:45 -0500

    refactor(qrdisplay): seperate html and javascript for qr display module, html template readable

--- src/util/qrdisplay.js
+++ src/util/qrdisplay.js
@@ -7,66 +7,32 @@ const getQRDataURI = (data) => {
 }
 
 const openQr = (data, cancel) => {
-    let uportQR = getUportQRDisplay()
-    uportQR.style.display = 'block'
-
-    let dataUri = getQRDataURI(data)
-    let qrImg = uportQR.children[0].children[0]
-    let cancelButton = uportQR.children[0].children[2]
-    cancelButton.addEventListener('click', (event) => { cancel() })
-    qrImg.setAttribute('src', dataUri)
-  }
+  let wrapper = document.createElement('div')
+  wrapper.setAttribute('id', 'uport-wrapper')
+  wrapper.innerHTML = uportQRDisplay({qrImageUri: getQRDataURI(data), cancel})
+  const cancelClick =  (event) => { document.getElementById('uport-qr-text').innerHTML = 'Cancelling'; cancel() }
+  document.body.appendChild(wrapper)
+  document.getElementById('uport-qr-cancel').addEventListener('click', cancelClick)
+}
 
 const closeQr = () => {
-    let uportQR = getUportQRDisplay()
-    uportQR.style.display = 'none'
-    resetQRCancellation()
-  }
+  const uportWrapper = document.getElementById('uport-wrapper')
+  document.body.removeChild(uportWrapper)
+}
 
-const resetQRCancellation = () => {
-    document.getElementById('uport-qr-text').innerHTML = 'Please scan with uport app'
-  }
-
-const getUportQRDisplay = () => {
-    let bg = document.getElementById('uport-qr')
-    if (bg) return bg
-
-    bg = document.createElement('div')
-    bg.setAttribute('id', 'uport-qr')
-    bg.setAttribute('style', 'position:fixed;top: 0;width:100%;height:100%;z-index:100;background-color:rgba(0,0,0,0.5);text-align:center;')
-
-    let box = document.createElement('div')
-    box.setAttribute('style', 'position:relative;top:50%;display:inline-block;z-index:101;background:#fff;transform:translateY(-50%);margin:0 auto;padding:20px')
-
-    let text = document.createElement('p')
-    text.innerHTML = 'Please scan with uport app'
-    text.id = 'uport-qr-text'
-
-    let cancelButton = document.createElement('button')
-    cancelButton.innerHTML = 'Cancel'
-    cancelButton.addEventListener('click', function(event) {
-      document.getElementById('uport-qr-text').innerHTML = 'Cancelling';
-    });
-
-    let qrImg = document.createElement('img')
-    qrImg.setAttribute('style', 'z-index:102;')
-
-    box.appendChild(qrImg)
-    box.appendChild(text)
-    box.appendChild(cancelButton)
-    bg.appendChild(box)
-    document.body.appendChild(bg)
-
-    // let test = document.getElementById('uport-qr-text')
-    // test.innerHTML = 'testing...'
-
-    return bg
-  }
+const uportQRDisplay = ({qrImageUri}) => `
+  <div id="uport-qr" style="position:fixed;top: 0;width:100%;height:100%;z-index:100;background-color:rgba(0,0,0,0.5);text-align:center;">
+    <div style="position:relative;top:50%;display:inline-block;z-index:101;background:#fff;transform:translateY(-50%);margin:0 auto;padding:20px">
+      <img style="z-index:102;" src="${qrImageUri}"/>
+      <p id="uport-qr-text"> Please scan with uPort app </p>
+      <button id="uport-qr-cancel"> Cancel </button>
+    </div>
+  </div>
+`
 
 export {
-  getUportQRDisplay,
-  resetQRCancellation,
   closeQr,
   openQr,
-  getQRDataURI
+  getQRDataURI,
+  uportQRDisplay
 }
