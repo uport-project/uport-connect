009a8e96593d00de7ca033e26d6a0afe674d4106
Christian Lundkvist <christian.lundkvist@gmail.com>
2017-07-16 17:15:25 -0400

    feat(tutorial): Fix MNID/address issue with getCoinbase

--- tutorial/uport_tutorial.js
+++ tutorial/uport_tutorial.js
@@ -7,6 +7,9 @@ const appName = 'UportTutorial'
 const connect = new Connect(appName, {network: 'rinkeby'})
 const web3 = connect.getWeb3()
 
+
+// Setup the simple Status contract - allows you to set and read a status string
+
 const abi = [{"constant":false,"inputs":[{"name":"status","type":"string"}],"name":"updateStatus","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"addr","type":"address"}],"name":"getStatus","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"}]
 
 const StatusContract = web3.eth.contract(abi);
@@ -16,8 +19,11 @@ const statusInstance = StatusContract.at('0x70A804cCE17149deB6030039798701a38667
 const uportConnect = function () {
   web3.eth.getCoinbase((error, address) => {
     if (error) { throw error }
-    globalState.uportId = address
-    globalState.ethAddress = window.uportconnect.MNID.decode(globalState.uportId).address
+    globalState.ethAddress = address
+
+    // This one is for display purposes - MNID encoding includes network 
+    globalState.uportId = window.uportconnect.MNID.encode({network: '0x4', address: address})
+
     statusInstance.getStatus.call(globalState.ethAddress, (err, st) => {
       globalState.currentStatus = st
       web3.eth.getBalance(globalState.ethAddress, (err, bal) => {
@@ -31,15 +37,11 @@ const uportConnect = function () {
 // Send ether
 const sendEther = () => {
   const value = parseFloat(globalState.sendToVal) * 1.0e18
-  const gasPrice = 100000000000
-  const gas = 500000
 
   web3.eth.sendTransaction(
     {
       to: globalState.sendToAddr,
-      value: value,
-      gasPrice: gasPrice,
-      gas: gas
+      value: value
     },
     (error, txHash) => {
       if (error) { throw error }
