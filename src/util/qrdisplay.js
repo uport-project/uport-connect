30630263e09356a7d38010878c9cccff68839fc4
Jeff Scott Ward <jsward.17@gmail.com>
2017-10-06 15:13:45 -0400

    big refactor - assests - instructions - animated

--- src/util/qrdisplay.js
+++ src/util/qrdisplay.js
@@ -26,13 +26,20 @@ const getQRDataURI = (data) => {
  *  @param    {Function}   cancel    a function called when the cancel button is clicked
  */
 const openQr = (data, cancel, appName, introModal) => {
+  
   // in the future can change call of action based on type of request
   let wrapper = document.createElement('div')
   wrapper.setAttribute('id', 'uport-wrapper')
 
-  wrapper.innerHTML = introModal ? introModalDisplay(appName) : uportQRDisplay({qrImageUri: getQRDataURI(data), cancel})
+  wrapper.innerHTML = 
+    introModal 
+      ? introModalDisplay(appName)
+      : uportQRDisplay({qrImageUri: getQRDataURI(data), cancel})
 
-  const cancelClick = (event) => { document.getElementById('uport-qr-text').innerHTML = 'Cancelling'; cancel() }
+  const cancelClick = (event) => { 
+    document.getElementById('uport-qr-text').innerHTML = 'Cancelling'; cancel() 
+  }
+  
   const uportTransition = (event) => {
     wrapper.innerHTML = uportQRDisplay({qrImageUri: getQRDataURI(data), cancel})
     document.getElementById('uport-qr-cancel').addEventListener('click', cancelClick)
@@ -51,6 +58,40 @@ const closeQr = () => {
   document.body.removeChild(uportWrapper)
 }
 
+const introModalDisplay = (appName) => {
+  let content = `
+    <div style="${uportModalIntroWrapper}">`
+      if (appName)  {
+        content +=  `
+          <div>
+            <p id="uport-qr-text" style="${uportQRTextWithAppName}">
+              <span>Login Into</span>
+              <span> </span>
+              <span style="${uportAppName}">${appName}</span>
+            </p>
+          </div>`
+      }
+      content +=  `
+        <div id="uport-continue-btn" style="${uportModalContinueBtn}"> 
+          <span style="${uportModalLogo}">${SVG.logo}</span>
+          <span>&nbsp;&nbsp;</span>
+          <span>Continue with uPort</span>
+        </div>
+
+    </div>
+
+    <div style="${uportModalNewUserFooterCSS}">
+      <p style="${uportModalNewUserFooterTitleCSS}">New uPort User?</p>
+      <div style="${uportModalNewUserFooterAppStoresCSS}">
+        <div style="${uportModalNewUserFooterAppStoresAndroidCSS}">${SVG.androidApp}</div>
+        <div style="${uportModalNewUserFooterAppStoresiOSCSS}">${SVG.appleApp}</div>
+      </div>
+    </div>
+  `
+
+  return uportModal(content)
+}
+
 /**
  *  A html pop over QR display template
  *
@@ -59,46 +100,181 @@ const closeQr = () => {
  *  @return   {String}                        a string of html
  */
 const uportQRDisplay = ({qrImageUri}) => uportModal(`
-  <div style="padding: 15px 45px 35px 45px;">
-    <div style="background-color: #000; width: 60px; height: 60px; margin: 10px auto 25px auto;"> </div>
-    <p></p>
-    <p id="uport-qr-text" style="color: #7C828B; font-family: Avenir; font-size: 18px; text-align: center; margin-top: 10px;"> Scan QR code with uPort Mobile App </p>
-    <img style="z-index:102;" src="${qrImageUri}"/>
-  </div>`)
-
-const introModalDisplay = (appName) => {
-    let content = `
-      <div style="padding: 40px 70px 40px 70px;"> `
-      if (appName)  {
-        content +=  `<p id="uport-qr-text" style="margin-bottom: 30px; font-size: 18px; color: #7C828B; font-family: Avenir;"> Login Into <span style="font-weight: 700;"> ${appName} </span> </p>`
-      }
-      content +=  `
-        <div id="uport-continue-btn" style="${buttonStyleBlue}"> Continue with uPort </div>
-      </div>
-      <div style="background-color: #F6F7F8;; padding: 16px; min-height: 110px;">
-        <p style="font-size: 14px; color: #7C828B; font-family: Avenir;"> New uPort User? </p>
-        <div class="app-store-container" style="padding: 0px 42px;">
-          <div style="background-color: #000; width: 128px; height: 40px; margin-right: 20px; float:left;"> </div>
-          <div style="background-color: #000; width: 128px; height: 40px; float: left"> </div>
-        </div>
-      </div>`
-
-    return uportModal(content)
-}
+  <div>
+    <div style="${uportLogoWithBg}">${SVG.logowithBG}</div>
+    <p id="uport-qr-text" style="${uportQRInstructions}">Scan QR code with uPort Mobile App</p>
+    <img src="${qrImageUri}" style="${uportQRIMG}" />
+  </div>
+`)
 
 // An empty modal
 const uportModal = (innerHTML) => `
-  <div id="uport-qr" style="position:fixed;top: 0;width:100%;height:100%;z-index:100;background-color:rgba(0,0,0,0.5);text-align:center;">
-    <div style="position:relative;top:50%;display:inline-block;z-index:101;background:#fff;transform:translateY(-50%);margin:0 auto; border-radius: 8px; overflow: hidden; box-shadow: 0 12px 24px 0 rgba(0,0,0,0.1);">
-      <div style="width: 100%; height: 45px;">
-        <div id="uport-qr-cancel" style="float: right; background-color: #000; height: 25px; width: 25px; margin: 15px; cursor: pointer;"> </div>
+  <div id="uport-qr" style="${uportQRCSS}">
+    <div style="${uportModalCSS}" class="animated fadeIn">
+      <div style="${uportModalHeaderCSS}">
+        <div id="uport-qr-cancel" style="${uportModalHeaderCloseCSS}">
+          ${SVG.close}
+        </div>
       </div>
-      ${innerHTML}
+      <div style="${uportModalConentCSS}">
+        ${innerHTML}
+      </div>
     </div>
-  </div>`
+    ${animateCSS}
+  </div>
+`
+const animateCSS = `
+<style>
+  @keyframes fadeIn {
+    from { opacity: 0; }
+    to { opacity: 1; }
+  }
+  .animated {
+    animation-duration: 1s;
+    animation-fill-mode: both;
+  }
+  .fadeIn {
+    animation-name: fadeIn;
+  }
+</style>
+`
+
+const uportQRCSS = `
+  position:fixed;
+  top: 0;
+  width:100%;
+  height:100%;
+  z-index:100;
+  background-color:rgba(0,0,0,0.5);
+  text-align:center;
+`
+
+const uportModalCSS = `
+  position:relative;
+  top:50%;
+  display:inline-block;
+  z-index:101;
+  background:#fff;
+  transform:translateY(-50%);
+  margin:0 auto;
+  border-radius: 8px;
+  overflow: hidden;
+  box-shadow: 0 12px 24px 0 rgba(0,0,0,0.1);
+  min-width: 400px;
+`
+
+const uportModalHeaderCSS = `
+  width: 100%;
+  height: 45px;
+`
+
+const uportModalHeaderCloseCSS = `
+  float: right;
+  height: 25px;
+  width: 25px;
+  margin: 15px;
+  cursor: pointer;
+`
+
+const uportModalConentCSS = `
+`
+
+const uportModalNewUserFooterCSS = `
+  background-color: #F6F7F8;
+  padding: 26px 0;
+  min-height: 110px;
+`
+const uportModalNewUserFooterTitleCSS = `
+  font-size: 14px;
+  color: #7C828B;
+  font-family: Avenir;
+`
+
+const uportModalNewUserFooterAppStoresCSS = `
+  padding: 10px 42px;
+`
+
+const uportModalNewUserFooterAppStoresAndroidCSS = `
+  width: 128px;
+  height: 40px;
+  margin-right: 20px;
+  display: inline-block;
+`
+
+const uportModalNewUserFooterAppStoresiOSCSS = `
+  width: 128px;
+  height: 40px;
+  display: inline-block;
+`
+
+const uportModalLogo = `
+  display:inline-block;
+  max-width: 50px;
+  vertical-align: middle;
+`
+
+const uportAppName = `
+  font-weight: 700;
+`
+
+const uportQRTextWithAppName = `
+  font-size: 18px;
+  color: #7C828B;
+  font-family: Avenir;
+`
+
+const uportLogoWithBg = `
+  width: 60px;
+  height: 60px;
+  margin: 0 auto 0 auto;
+`
+
+const uportQRInstructions = `
+  color: #7C828B;
+  font-family: Avenir;
+  font-size: 18px;
+  text-align: center;
+  margin-top: 0;
+`
+
+const uportModalIntroWrapper = `
+  text-align: center;
+  display: inline-block;
+  width: 100%;"
+`
+
+const uportQRIMG = `
+  z-index:102;
+  margin-bottom: 35px;
+`
 
 // TODO space for logo in here already
-const buttonStyleBlue = `text-align: center;padding: 17px 25px 17px 68px;border-radius: 6px;color: #fff;margin: 10px;font-family: arial, sans-serif;font-weight: 500;letter-spacing: 0.8px;border-color: #4f45af;text-shadow: none;background-color: #5C50CA;background-image:  url(${SVG.logo});background-position: left 18px bottom 11px;background-repeat: no-repeat;border: 1px solid #ccc;cursor: pointer;display: inline-block;position: relative;white-space: nowrap;box-sizing: border-box;font-size: 16px;text-decoration: noneuser-select: none;transition: border-color 0.1s linear,background 0.1s linear,color 0.1s linear;-o-transition: border-color 0.1s linear,background 0.1s linear,color 0.1s linear;-ms-transition: border-color 0.1s linear,background 0.1s linear,color 0.1s linear;`
+const uportModalContinueBtn = `
+  text-align: center;
+  padding: 17px 25px 17px 25px;
+  border-radius: 6px;
+  color: #fff;
+  margin: 75px auto 80px 0;
+  font-family: arial, sans-serif;
+  font-weight: 500;
+  letter-spacing: 0.8px;
+  border-color: #4f45af;
+  text-shadow: none;
+  background-color: #5C50CA;
+  background-position: left 18px bottom 11px;
+  background-repeat: no-repeat;
+  border: 1px solid #ccc;
+  cursor: pointer;
+  display: inline-block;
+  position: relative;
+  white-space: nowrap;
+  box-sizing: border-box;
+  font-size: 16px;
+  text-decoration: noneuser-select: none;
+  transition: border-color 0.1s linear,background 0.1s linear,color 0.1s linear;
+  -o-transition: border-color 0.1s linear,background 0.1s linear,color 0.1s linear;
+  -ms-transition: border-color 0.1s linear,background 0.1s linear,color 0.1s linear;
+`
 
 export {
   closeQr,
