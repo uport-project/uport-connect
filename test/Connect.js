87532b377d28d94618dc09bb181540f8d6b21be7
Zach Ferland <zachferland@gmail.com>
2018-05-24 15:57:39 -0400

    test: add test coverage for onResponse, request, contract, sendTransaction, (de)serialize, set/getState, and fix other tests

--- test/Connect.js
+++ test/Connect.js
@@ -15,6 +15,10 @@ const getURLJWT = (url) => url.replace(/https:\/\/id.uport.me\/req\//, '').repla
 
 describe('Connect', () => {
 
+  beforeEach(()=>{
+    window.localStorage.clear()
+  })
+
   describe('constructor', () => {
     it('sets defaults', () => {
       const uport = new Connect('test app')
@@ -53,7 +57,6 @@ describe('Connect', () => {
     })
 
     it('creates keypair if none in local storage', () => {
-      window.localStorage.clear()
       expect(window.localStorage.getItem('connectState')).to.equal(null)
       const uport = new Connect('test app')
       expect(!!uport.keypair.did).to.be.true
@@ -209,64 +212,132 @@ describe('Connect', () => {
 
 
   describe('onResponse', () => {
-    it('resolves once a response with given id available', () => {
-      // Sim hash change
+    const id = 'test'
+    const JWTParse = {'name': 'uPort Demo', '@type': 'App', 'description': 'Demo App', 'url': 'demo.uport.me', 'address': 'did:uport:2oeXufHGDpU51bfKBsZDdu7Je9weJ3r7sVG'}
+    const did = 'did:uport:2oeXufHGDpU51bfKBsZDdu7Je9weJ3r7sVG'
+    const JWTReq = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NksifQ.eyJpYXQiOjE1MjcxODM0ODcsImV4cCI6MTUyNzE4NDA4NywicmVxdWVzdGVkIjpbIm5hbWUiLCJwaG9uZSIsImNvdW50cnkiXSwicGVybWlzc2lvbnMiOlsibm90aWZpY2F0aW9ucyJdLCJjYWxsYmFjayI6Imh0dHBzOi8vY2hhc3F1aS51cG9ydC5tZS9hcGkvdjEvdG9waWMvS3JzRkxnSDFQa3RwOGZ0eSIsInR5cGUiOiJzaGFyZVJlcSIsImlzcyI6ImRpZDp1cG9ydDoyb2VYdWZIR0RwVTUxYmZLQnNaRGR1N0plOXdlSjNyN3NWRyJ9.bJC2dWT0tCdFOeC0JlN_Dx9PwyI18wtVHz2MOp-9I7QPNhgA8SlbqdqJiMrmZfc1PdM3AjNVD31HmuDoQYQMNQ'
+
+    it('resolves once a response with given id available from pub', (done) => {
+      const response = { res: 'test', data: ''}
+      const uport = new Connect('testApp')
+      uport.onResponse(id).then((res) => {
+        expect(res.res).to.equal('test')
+        done()
+        return
+      })
+      uport.PubSub.publish(id,response)
     })
 
-    it('handles JWT responses, by parsing, validating and returning', () => {
-      // Sim hash change or jwt
-      // sim parsing/validating?
+    it('resolves once a response with given id available from hash change', (done) => {
+      const id = 'test'
+      const uport = new Connect('testApp')
+      uport.onResponse(id).then((res) => {
+        // TODO move to vars above
+        expect(res.res).to.equal('0x00521965e7bd230323c423d96c657db5b79d099f')
+        done()
+      })
+      window.location.hash = `access_token=0x00521965e7bd230323c423d96c657db5b79d099f&id=test`
     })
 
-    it('handles tx hashes response, by just returning them', () => {
-      // Sim tx hash
-      // just return
+    it('handles JWT responses, parses, validates and returns by calling verifyResponse/credentials', (done) => {
+      const uport = new Connect('testApp')
+      const verifyResponse = sinon.stub().callsFake((jwt) => Promise.resolve(JWTParse))
+      uport.verifyResponse = verifyResponse
+      uport.onResponse(id).then((res) => {
+        expect(verifyResponse).to.be.called
+        expect(res.res).to.deep.equal(JWTParse)
+        done()
+      })
+      window.location.hash = `access_token=${JWTReq}&id=${id}`
     })
 
-    it('returns instantly if there is already a response on page load', () => {
-      // Instantiate
-      // set item
-      // call onload
+    it('handles tx hashes response, by just returning them', (done) => {
+      const uport = new Connect('testApp')
+      uport.onResponse(id).then((res) => {
+        expect(res.res).to.equal('0x00521965e7bd230323c423d96c657db5b79d099f')
+        done()
+      })
+      window.location.hash = `access_token=0x00521965e7bd230323c423d96c657db5b79d099f&id=test`
     })
 
-    it('it writes to local storage (mnid, doc, storage) if values already not available', () => {
-      // simulate repsonse
-      // check if local storage updated
+    it('returns instantly if there is already a response on connect instantiation', () => {
+      // Set response before connect instantiate
+      window.location.hash = `access_token=0x00521965e7bd230323c423d96c657db5b79d099f&id=test`
+      const uport = new Connect('testApp')
+      return uport.onResponse(id).then((res) => {
+        expect(res.res).to.equal('0x00521965e7bd230323c423d96c657db5b79d099f')
+        return
+      })
     })
+
+    it('it writes to local storage and instance (did, ...) if values already not available', (done) => {
+      const uport = new Connect('testApp')
+      uport.verifyResponse = sinon.stub().callsFake((jwt) => Promise.resolve(JWTParse))
+      uport.onResponse(id).then((res) => {
+        expect(uport.did).to.equal(did)
+        const connectState = window.localStorage.getItem('connectState')
+        expect(new RegExp(`${did}`).test(connectState)).to.be.true
+        done()
+      })
+      window.location.hash = `access_token=${JWTReq}&id=${id}`
+    })
+
+    // TODO test error handling
   })
-
 
   describe('request', () => {
     it('calls mobile transport if on mobile client', () => {
-      // mobile transport spy
+      const mobileTransport = sinon.stub().callsFake(() => new Promise((resolve)=> resolve()))
+      const uport = new Connect('testApp', { mobileTransport, isMobile: true})
+      uport.request('uri', 'id')
+      expect(mobileTransport).to.be.called
+      expect(mobileTransport).to.be.calledWith('uri')
     })
 
     it('calls transport if on desktop client', () => {
-      // transport spie
+      const transport = sinon.stub().callsFake(() => new Promise((resolve)=> resolve('test')))
+      const uport = new Connect('testapp', { transport, isMobile: false})
+      uport.request('uri', 'id')
+      expect(transport).to.be.called
+      expect(transport).to.be.calledWith('uri')
     })
 
-    it('requires a request id', () => {
-      // Not requst id, throws error
+    it('requires a request id, throws error if none', () => {
+        const uport = new Connect('testapp')
+        try { uport.request() } catch(err) { return }
+        throw new Error('Func should have thrown')
     })
 
-    it('on desktop client it publishes response to subscriber once returned', () => {
-      // Either spy to see if pubsub called, or set pubsub sub to see if returned
+    it('on desktop client it publishes response to subscriber once returned', (done) => {
+      const transport = sinon.stub().callsFake(() => new Promise((resolve)=> resolve('test')))
+      const uport = new Connect('testapp', { transport, isMobile: false})
+      // Check if publish func called, by subsribing to event
+      uport.PubSub.subscribe('topic', (msg, res) => {
+        expect(res).to.equal('test')
+        done()
+      })
+      uport.request('request', 'topic')
     })
   })
 
   describe('contract', () => {
     // NOTE: most unit test are in uport-js/core
-    it('returns contract object with txobj handler and abi configured', () => {
-      // Can these be checked?
-    })
-
-    it('calls sendTransaction with txobj and id on method call', () => {
-      // Spy on sendtransaction, and check if called with both and id and txobj
+    it('returns contract object with txobj handler (sendTransaction) and abi configured', () => {
+      const uport = new Connect('testapp')
+      const sendTransaction = sinon.spy()
+      uport.sendTransaction = sendTransaction
+      const abi = [{"constant":false,"inputs":[{"name":"status","type":"string"}],"name":"updateStatus","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"addr","type":"address"}],"name":"getStatus","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"}]
+      const Contract = uport.contract(abi).at('0x00521965e7bd230323c423d96c657db5b79d099f')
+      expect(Contract.abi).to.deep.equal(abi)
+      // sendTransaction configured as TxObj handler if called on function call
+      Contract.updateStatus('hello')
+      expect(sendTransaction).to.be.called
     })
   })
 
 
   describe('sendTransaction', () => {
+    // TODO will finish once available in uport-js
     it('call request with request uri including transaction jwt', () => {
     })
 
@@ -284,23 +355,68 @@ describe('Connect', () => {
 
   describe('serialize', () => {
     it('returns string representing all persistant state of connect obj', () => {
+      const uport = new Connect('testapp')
+      uport.address = '0x00521965e7bd230323c423d96c657db5b79d099f'
+      uport.mnid = '2oeXufHGDpU51bfKBsZDdu7Je9weJ3r7sVG'
+      uport.doc = {name: 'Ran'}
+      uport.did = 'did:uport:2oeXufHGDpU51bfKBsZDdu7Je9weJ3r7sVG'
+      uport.firstReq = true
+      uport.keypair = {did: 'did:ethr:0x413daa771a2fc9c5ae5a66abd144881ef2498c54' , keypair: '1338f32fefb4db9b2deeb15d8b1b428a6346153cc43f51ace865986871dd069d'}
+      const uportStateString = uport.serialize()
+      expect(uportStateString).to.be.a('string')
+      expect(/0x00521965e7bd230323c423d96c657db5b79d099f/.test(uportStateString)).to.be.true
+      expect(/2oeXufHGDpU51bfKBsZDdu7Je9weJ3r7sVG/.test(uportStateString)).to.be.true
+      expect(/Ran/.test(uportStateString)).to.be.true
+      expect(/true/.test(uportStateString)).to.be.true
+      expect(/did:ethr:0x413daa771a2fc9c5ae5a66abd144881ef2498c54/.test(uportStateString)).to.be.true
+      expect(/1338f32fefb4db9b2deeb15d8b1b428a6346153cc43f51ace865986871dd069d/.test(uportStateString)).to.be.true
     })
   })
 
 
   describe('deserialize', () => {
     it('sets all persitant state of connect object given serialized string of state', () => {
+      const uportTest = new Connect('testapp')
+      uportTest.address = '0x00521965e7bd230323c423d96c657db5b79d099f'
+      uportTest.mnid = '2oeXufHGDpU51bfKBsZDdu7Je9weJ3r7sVG'
+      uportTest.doc = {name: 'Ran'}
+      uportTest.did = 'did:uport:2oeXufHGDpU51bfKBsZDdu7Je9weJ3r7sVG'
+      uportTest.firstReq = true
+      uportTest.keypair = {did: 'did:ethr:0x413daa771a2fc9c5ae5a66abd144881ef2498c54' , keypair: '1338f32fefb4db9b2deeb15d8b1b428a6346153cc43f51ace865986871dd069d'}
+
+      const uport = new Connect('testapp')
+      uport.deserialize(uportTest.serialize())
+
+      expect(uport.address).to.equal(uportTest.address)
+      expect(uport.mnid).to.equal(uportTest.mnid)
+      expect(uport.doc).to.deep.equal(uportTest.doc)
+      expect(uport.did).to.equal(uportTest.did)
+      expect(uport.firstReq).to.equal(uportTest.firstReq)
+      expect(uport.keypair).to.deep.equal(uportTest.keypair)
     })
   })
 
   describe('getState', () => {
     it('gets serialized state from local storage and calls deserialize with it', () => {
+      const uport = new Connect('testapp')
+      const deserialize = sinon.spy()
+      uport.deserialize = deserialize
+      // Set after connect instantiated
+      window.localStorage.setItem('connectState', 'uportTestStateString')
+      uport.getState()
+      expect(deserialize).to.be.calledWith('uportTestStateString')
     })
   })
 
 
   describe('setState', () => {
-    it('writes serialized state to local storage', () => {
+    it('writes serialized state to local storage at connectState ', () => {
+        const uport = new Connect('testapp')
+        const serialize = sinon.stub().callsFake(() => 'uportTestStateString')
+        uport.serialize = serialize
+        uport.setState()
+        expect(serialize).to.be.called
+        expect(window.localStorage.getItem('connectState')).to.equal('"uportTestStateString"')
     })
   })
 })
