45fd345cc288fd5647c929c07d9f8d4832f8d446
Zach Ferland <zachferland@gmail.com>
2018-06-01 14:59:27 -0400

    docs: update tutorial

--- examples/integration-tutorial/index.js
+++ examples/integration-tutorial/index.js
@@ -2,36 +2,49 @@
 
 // Setup
 
-const Connect = window.uportconnect.Connect
-const appName = 'UportTutorial'
-const connect = new Connect(appName, {network: 'rinkeby'})
-const web3 = connect.getWeb3()
+const Connect = window.uportconnect
+const yourAppName = 'UportTutorial'
+const connect = new Connect(yourAppName, {network: 'rinkeby'})
+const provider = connect.getProvider()
+// provider.getAddress((err, res) => {console.log(err);console.log(res)})
+    const web3 = new Web3()
+    web3.setProvider(provider)
+    // // Work around to issue with web3 requiring a from parameter. This isn't actually used.
+    // web3.eth.defaultAccount = '0xB42E70a3c6dd57003f4bFe7B06E370d21CDA8087'
 
+// console.log(provider)
+// const web3 = new Web3()
+// web3.setProvider(provider)
+// console.log(web3)
+// web3.eth.defaultaccount = '0x71845bbfe5ddfdb919e780febfff5eda62a30fdc'
 
 // Setup the simple Status contract - allows you to set and read a status string
 
 const abi = [{"constant":false,"inputs":[{"name":"status","type":"string"}],"name":"updateStatus","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"addr","type":"address"}],"name":"getStatus","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"}]
-
-const StatusContract = web3.eth.contract(abi);
-const statusInstance = StatusContract.at('0x70A804cCE17149deB6030039798701a38667ca3B')
+//
+const StatusContract = connect.contract(abi);
+const statusInstance = StatusContract.at('0x71845bbfe5ddfdb919e780febfff5eda62a30fdc')
+// console.log(statusInstance)
 
 // uPort connect
 const uportConnect = function () {
-
+  // connect.requestAddress()
   web3.eth.getCoinbase((error, address) => {
-    if (error) { throw error }
+    console.log(error)
+    console.log(address)
+    // if (error) { throw error }
     globalState.ethAddress = address
 
     // This one is for display purposes - MNID encoding includes network
     globalState.uportId = window.uportconnect.MNID.encode({network: '0x4', address: address})
 
-    statusInstance.getStatus.call(globalState.ethAddress, (err, st) => {
-      globalState.currentStatus = st
-      web3.eth.getBalance(globalState.ethAddress, (err, bal) => {
-        globalState.ethBalance = web3.fromWei(bal)
-        render()
-      })
-    })
+    // statusInstance.getStatus.call(globalState.ethAddress, (err, st) => {
+    //   globalState.currentStatus = st
+    //   web3.eth.getBalance(globalState.ethAddress, (err, bal) => {
+    //     globalState.ethBalance = web3.fromWei(bal)
+    //     render()
+    //   })
+    // })
   })
 }
 
@@ -57,12 +70,16 @@ const sendEther = () => {
 const setStatus = () => {
 
   const newStatusMessage = globalState.statusInput
-
-  statusInstance.updateStatus(newStatusMessage,
-                           (error, txHash) => {
-                             if (error) { throw error }
-                             globalState.txHashSetStatus = txHash
-                             render()
-                           })
+  statusInstance.updateStatus(newStatusMessage, 'updateStatus')
+  connect.onResponse('updateStatus').then(payload => {
+    globalState.txHashSetStatus = payload.res
+    render()
+  })
+  // statusInstance.updateStatus(newStatusMessage,
+  //                          (error, txHash) => {
+  //                            if (error) { throw error }
+  //                            globalState.txHashSetStatus = txHash
+  //                            render()
+  //                          })
 
 }
