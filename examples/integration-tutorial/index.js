376bad25f1d24b8bf80783db46ef276503db515a
Zach Ferland <zachferland@gmail.com>
2018-08-13 15:59:35 -0400

    feat: load state example, doc cleanup, request -> send, verified get/set, cb option onresponse

--- examples/integration-tutorial/index.js
+++ examples/integration-tutorial/index.js
@@ -9,30 +9,36 @@ const web3 = new Web3(provider)
 // Work around to issue with web3 requiring a from parameter. This isn't actually used.
 web3.eth.defaultAccount = '0xB42E70a3c6dd57003f4bFe7B06E370d21CDA8087'
 
+const abi = [{"constant":false,"inputs":[{"name":"status","type":"string"}],"name":"updateStatus","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"addr","type":"address"}],"name":"getStatus","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"}]
+const StatusContract = web3.eth.contract(abi).at('0x70A804cCE17149deB6030039798701a38667ca3B')
+
+const getAndRenderBlockchainState = (address) => {
+  StatusContract.getStatus.call(address, (err, status) => {
+    globalState.currentStatus = status
+    web3.eth.getBalance(address, (err, bal) => {
+      globalState.ethBalance = web3.fromWei(bal)
+      render()
+    })
+  })
+}
+
+// If state already availabe in connect onload, render it
 if (connect.did) {
   globalState.ethAddress = connect.address
   globalState.uportId = connect.did
   render()
+  getAndRenderBlockchainState(globalState.ethAddress)
 }
 
-const abi = [{"constant":false,"inputs":[{"name":"status","type":"string"}],"name":"updateStatus","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"addr","type":"address"}],"name":"getStatus","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"}]
-const StatusContract = web3.eth.contract(abi).at('0x70A804cCE17149deB6030039798701a38667ca3B')
 
 // uPort connect
 const uportConnect = function () {
   web3.eth.getCoinbase((error, address) => {
     if (error) { throw error }
     globalState.ethAddress = address
-    // This one is for display purposes - MNID encoding includes network
     globalState.uportId = connect.did
-
-    StatusContract.getStatus.call(globalState.ethAddress, (err, st) => {
-      globalState.currentStatus = st
-      web3.eth.getBalance(globalState.ethAddress, (err, bal) => {
-        globalState.ethBalance = web3.fromWei(bal)
-        render()
-      })
-    })
+    render()
+    getAndRenderBlockchainState(globalState.ethAddress)
   })
 }
 
